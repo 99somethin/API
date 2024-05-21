697684758cc347961a7b512fd0b7fa9907c72c5e
99somethin <igalovich@yandex.ru>
2024-05-22 00:23:01 +0300

    mainprog

---
+++ Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using ApiStudy.Data;
+using ApiStudy.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiStudy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext appDbContext;
+
+        public UserController(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<User>>> Get() => Ok(await appDbContext.Users.ToListAsync());
+
+        [HttpPost]
+
+        public async Task<ActionResult<User>> Add(User user)
+        {
+            if (user != null)
+            {
+                var result = appDbContext.Users.Add(user).Entity;
+
+                await appDbContext.SaveChangesAsync();
+
+                return Ok(result);
+            }
+            return BadRequest("Invalid Request");
+        }
+
+        [HttpGet ("{email}/{password}")]
+
+        public async Task<ActionResult<User>> Login(string email,string password )
+        {
+            if(email != null || password != null)
+            {
+                var user = await appDbContext.Users.Where(x => x.Email!.ToLower().Equals(email.ToLower()) && x.Password == password).FirstOrDefaultAsync();
+
+                return user != null ? Ok(user) : NotFound("User not found");
+                
+            }
+            return BadRequest("Invalid Request");
+        }
+
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiStudy.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiStudy.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; } = default!;
+    }
+}
